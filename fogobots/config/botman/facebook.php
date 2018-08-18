131689e65a6de09e5db6ff9c76874d01cfb4550c
tomahock <eu@tomahock.com>
2018-08-18 01:01:44 +0100

    try other things

--- fogobots/config/botman/facebook.php
+++ fogobots/config/botman/facebook.php
@@ -76,7 +76,7 @@ return [
     'persistent_menu' => [
         [
             'locale' => 'default',
-            'composer_input_disabled' => 'true',
+            'composer_input_disabled' => 'false',
             'call_to_actions' => [
                 [
                     'title' => 'Ajuda',
